9aa656e5f97b9a3905be39c2e79fc952f9b14dad
jabir-st-dev <127714535+jabir-st-dev@users.noreply.github.com>
2023-03-19 17:51:12 +0600

    implemented date, from, to, limit

--- database/users.js
+++ database/users.js
@@ -2,6 +2,7 @@ const connect = require('@databases/sqlite');
 const {sql} = require('@databases/sqlite');
 // We don't pass a file name here because we don't want to store
 // anything on disk
+
 const db = connect();
 
 async function prepare() {
@@ -16,7 +17,7 @@ async function prepare() {
     CREATE TABLE exercises (
       description VARCHAR NOT NULL,
       duration INT NOT NULL,
-      date VARCHAR NOT NULL,
+      date DATETIME NOT NULL,
       _id VARCHAR NOT NULL,
       FOREIGN KEY(_id) REFERENCES users(_id)
     );
@@ -61,50 +62,90 @@ exports.getAll = async function () {
     return users;
 }
 
-exports.setExercise = async function({_id, description, duration, date}) {
+exports.addExercise = async function ({_id, description, duration, date}) {
     await prepared;
-
-    const exerciseWithUsername = {};
-
-    await db.query(sql`
-    INSERT INTO exercises (_id, description, duration, date)
-      VALUES (${_id}, ${description}, ${duration}, ${date});
-  `);
 
     const user = await db.query(sql`
     SELECT * FROM users WHERE _id=${_id};
   `);
 
     if (user.length) {
-        exerciseWithUsername.username = user[0].username;
-    } else {
-        return undefined;
-    }
+        await db.query(sql`
+                INSERT INTO exercises (_id, description, duration, date)
+                  VALUES (${_id}, ${description}, ${duration}, ${date});
+              `);
+        const exercise = await db.query(sql`
+                SELECT * FROM exercises 
+                    WHERE _id=${_id} 
+                    AND description=${description}
+                    AND duration=${duration}
+                    AND date=${date};
+              `);
 
-    const exercise = await db.query(sql`
-    SELECT * FROM exercises 
-        WHERE _id=${_id} 
-        AND description=${description}
-        AND description=${description}
-        AND date=${date};
-  `);
-
-    if (exercise.length) {
-        return {
-            ...exerciseWithUsername,
-            ...exercise[0]
+        if (exercise.length) {
+            return {
+                username: user[0].username,
+                _id: exercise[0]._id,
+                description: exercise[0].description,
+                duration: exercise[0].duration,
+                date: new Date(exercise[0].date).toDateString()
+            }
+        } else {
+            return undefined;
         }
     } else {
         return undefined;
     }
 }
 
-exports.getAllExercise = async function({_id}) {
+exports.getAllExercises = async function ({_id, from, to, limit}) {
     await prepared;
 
-    const exercises = await db.query(sql`
-    SELECT * FROM exercises;
-  `);
+    console.log({_id, from, to, limit});
 
-    return exercises;
+    const user = await db.query(sql`
+            SELECT * FROM users WHERE _id=${_id};
+          `);
+
+    if (user.length) {
+        let exercises;
+
+        if (from.toDateString() !== 'Invalid Date' && to.toDateString() !== 'Invalid Date') {
+            exercises = await db.query(sql`
+                SELECT description, duration, date FROM exercises WHERE _id=${_id} 
+                AND date between ${from} AND ${to};
+              `);
+        } else {
+            exercises = await db.query(sql`
+                SELECT description, duration, date FROM exercises WHERE _id=${_id};
+              `);
+        }
+
+        for(let exercise of exercises) {
+            exercise.date = new Date(exercise.date).toDateString();
+        }
+
+        return {
+            username: user[0].username,
+            count: exercises.length,
+            _id,
+            log: exercises.slice(0, limit)
+        };
+    } else {
+        return undefined;
+    }
+};
+
+exports.seeAll = async function() {
+    await prepared;
+
+    const users = await db.query(sql`
+            SELECT * FROM users;
+          `);
+
+    const exercises = await db.query(sql`
+            SELECT * FROM exercises;
+          `);
+
+    return {users, exercises}
 }
--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const userDatabase = require('../database/users')
+const {getAllExercises} = require("../database/users");
 
 router.post('/', async function (req, res) {
     const user = await userDatabase.createUser({username: req.body.username});
@@ -31,23 +32,37 @@ router.post('/:_id/exercises', async function (req, res) {
         tDate = Date.now();
     }
 
-    const date = new Date(tDate).toDateString();
+    const date = new Date(tDate);
 
-    const exerciseWithUsername = await userDatabase.setExercise({_id, description, duration, date})
+    const exerciseWithUsername = await userDatabase.addExercise({_id, description, duration, date})
 
     console.log({exerciseWithUsername});
 
     res.send(exerciseWithUsername);
 });
 
-router.get('/:_id/logs', async function(req, res) {
+router.get('/:_id/logs', async function (req, res) {
     const {_id} = req.params;
+    const {limit} = req.query;
 
-    const exercises = await userDatabase.getAllExercise({_id});
+    const from = new Date(req.query.from);
+    const to = new Date(req.query.to);
 
-    console.log({exercises});
+    let exercises;
 
+    console.log({from, to, limit});
+
+    exercises = await userDatabase.getAllExercises({_id, from, to, limit});
+
+    // console.log({exercises});
     res.send(exercises);
+})
+
+router.get('/all', async function(req, res) {
+    const {users, exercises} = await userDatabase.seeAll();
+
+    console.log({users, exercises});
+    res.send({users, exercises});
 })
 
 module.exports = router;
